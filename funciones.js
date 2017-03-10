66669f8423fa7abe38ef3e1938049bcd15c1f396
Horoshi <gersonestuardo34@gmail.com>
2017-03-10 12:09:06 -0600

    examen de progra

---
+++ funciones.js
@@ -0,0 +1,13 @@
+var BacoB = angular.module("BacoB",[]);
+BacoB.controller("mensaje",function($scope){
+$scope.mensaje ="mensaje"
+});
+
+Bacob = angular.module("BacoB",[]);
+BacoB.controller("BacoB",function($scope)
+){;
+	$scope.first = 8;
+	$scope.second = 8;
+	
+	$scope.suma = scope.first + scope.second
+});
